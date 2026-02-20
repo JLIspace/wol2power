a72d3b0396b9cff80cb39d756c49febf3b475b8b
JLIspace <jli@jli.space>
2026-02-20 19:51:56 +0100

    Update power_script.sh
    Check if PC is off (based on power-button LED) before pressing the power button after receiving WOL message.

--- root/power_script.sh
+++ root/power_script.sh
@@ -1,8 +1,18 @@
-# Press down
-echo 1 > /sys/class/gpio/gpio503/value
+#!/bin/sh
+set -eu
 
-# Wait for 1 second
+LED_GPIO=504     # Power-LED
+BTN_GPIO=503     # Power-Button
+
+STATE=$(cat /sys/class/gpio/gpio$LED_GPIO/value)
+
+# Active-Low: 1 = PC off
+if [ "$STATE" != "1" ]; then
+  echo "PC already ON — ignoring"
+  exit 0
+fi
+
+# press power button
+echo 1 > /sys/class/gpio/gpio$BTN_GPIO/value
 sleep 1
-
-# Release
-echo 0 > /sys/class/gpio/gpio503/value
+echo 0 > /sys/class/gpio/gpio$BTN_GPIO/value
